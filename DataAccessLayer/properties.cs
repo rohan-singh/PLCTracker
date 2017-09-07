cbc3a4d8d50ada88f561ab07b2ad9a4b939da877
Rohan Singh <rohan.vishen@gmail.com>
2017-09-08 00:33:49 +0530

    Create properties.cs

---
+++ DataAccessLayer/properties.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class properties
+    {
+        public string ipaddress;
+        public string owner;
+        public string status;
+        public string user;
+    }
+}
